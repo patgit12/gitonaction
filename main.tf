18d9c25a9025198fbe5e12fb46842f412b9b4cc1
patongit <90884766+patgit12@users.noreply.github.com>
2023-09-20 13:00:56 -0400

    updating

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 resource "aws_vpc" "mynet" {
-cidr_block = "10.0.0.0/16"
-tags = {
+  cidr_block = "10.0.0.0/16"
+  tags = {
     Name = "action-test"
 
-}
+  }
 }
 provider "aws" {
   region = "us-east-1"
